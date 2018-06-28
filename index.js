c510f5d0b2d9211ac4b496f26b82c4b0c44f4516
Luis Garzon <luisgarzon90@yahoo.com>
2018-06-28 19:12:24 -0400

    created a simple if else statement

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('hello')
+const firstName = 'luis'
+
+const lastName = ' garzon'
+
+const fullname = firstName + lastName;
+
+
+console.log(fullname);
+
+
+const mikeAge = 25;
+
+var mikeName = 'mike';
+
+const steveAge = 26;
+
+var steveName = 'steve';
+
+if(mikeAge > steveAge) {
+    console.log(mikeName + ' is older than ' + steveName)
+} else if (steveAge > mikeAge) {
+    console.log(steveName + ' is older than ' + mikeName)
+} else if (mikeAge === steveAge){
+    console.log('both ' + mikeName + ' and ' + steveName + ' are the same age.' )
+}
+
+
+
+
