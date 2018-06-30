c447328e27446c00ac106903917e24d1cfd2e2e8
Luis Garzon <luisgarzon90@yahoo.com>
2018-06-29 21:21:37 -0400

    same as before

--- index.js
+++ index.js
@@ -29,7 +29,29 @@ function myFunction(){
 }
 console.log(myFunction())
 
+function tipCal(bill) {
 
+    var percentage;
+    
+    if(bill < 50){
+        percentage = .2;
+    } else if(bill >= 50 && bill <= 200){
+        percentage = .15;
+    }else{
+        percentage = .1;
+    }
+    return percentage * bill;
+}
+
+console.log(tipCal(10));
+
+var bills = [124, 48, 268];
+
+var tips = [ tipCal(bills[0]),
+            tipCal(bills[1]),
+            tipCal(bills[2]) ]
+
+console.log(tips);
 
 
 
